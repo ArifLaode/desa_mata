18346927f5d9ccdab91d1cb6419221cbece689f5
ChrishtoherRolad132 <mumadarif99@gmail.com>
2023-07-28 01:56:26 +0800

    modified:   db/db.js

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: '185.27.134.10',
+    host: 'ftpupload.net',
     user: 'if0_34701946',
     password: '#Aramata12',
-    database: 'des_tree'
+    database: 'des_tree',
 });
 
 db.connect((err) => {
