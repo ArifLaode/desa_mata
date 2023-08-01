6d956e8fab1a9ab1c75b8b219ebc503c80f1009e
Christpher Roand <92573638+ChrishtoherRolad132@users.noreply.github.com>
2023-08-02 00:18:38 +0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -6,8 +6,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.PORT
+    database: process.env.DB_DATABASE
 });
 
 db.connect((err) => {
